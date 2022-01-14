9936498ff63b0668e29c37106f11f7993b9adc6f
Jeffery <jefferywojo1998@gmail.com>
2022-01-13 18:39:00 -0600

    pushign schema

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+const { gql } = require('apollo-server-express');
+
+// define typedefs (based on assignment README)
+const typeDefs = gql`
+  }
+  type User {
+    _id: ID!
+    first_name: String!
+    last_name: String!
+    email: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs
