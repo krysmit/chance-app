9c2a7993f67c4ee7f7eb2b3ad88e3c0f24c0ffc1
Krystyn Smith <krskrsmith@gmail.com>
2022-01-15 20:04:54 -0600

    a few more syntax changes made to get sign up to work. not quite there yet but moving forward.

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { useMutation } from "@apollo/react-hooks";
