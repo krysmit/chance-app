b1781325d70c223f681c240ca6292b78024afc9e
Krystyn Smith <krskrsmith@gmail.com>
2022-01-18 20:04:49 -0600

    updated to fix heroku push issues

--- client/src/pages/SavedResume.js
+++ client/src/pages/SavedResume.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useQuery, useMutation } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { removeResumeId } from '../utils/localStorage';
